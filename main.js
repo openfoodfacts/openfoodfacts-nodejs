0d849311f7e005759eeeb490aa40e25afb3bc7a3
Mohit Sharma <msvbs98@gmail.com>
2018-02-13 16:12:32 +0530

    Add getProducts() function

---
+++ main.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const API_URL = 'https://world.openfoodfacts.org/'
+
+let OFF = {}
+
+OFF.getProduct = (barcode) => {
+	return new Promise((resolve,reject) => {
+		request(`${API_URL}api/v0/product/${barcode}.json`, (err, response, body) => {
+			if(err)
+				reject(err)
+			else
+			{
+				console.log(body)
+				resolve(JSON.parse(body))
+			}
+		})
+	})
+}
+

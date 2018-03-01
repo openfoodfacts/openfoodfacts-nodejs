604edc34a26ced68032687e21f8f0d05be6d6ade
Nigelk <kozlown@gmail.com>
2018-03-01 13:09:28 +0100

    feat(country): OFF is now set to be immutable
    The country method doesn't affect initial object anymore but it returns a new Instance of OFF.

--- example/examples.js
+++ example/examples.js
@@ -1,27 +1,24 @@
 const OFF = require('../main')
 
-const OFFAPI = new OFF()
+const worldOFF = new OFF()
 
-OFFAPI.country('india').getBrands().then(brands => {
-  console.info(brands)
-}).catch(error => console.error(error))
+const indiaOFF = worldOFF.country('in')
+const frenchOFF = worldOFF.country('fr')
 
-OFFAPI.getProduct(7622210288257).then(product => {
-  console.info(product)
-}).catch(error => console.error(error))
+async function examples () {
+  console.info((await indiaOFF.getBrands()).tags.length)
+  console.info((await frenchOFF.getBrands()).tags.length)
 
-OFFAPI.getBrand('monoprix').then(brand => {
-  console.info(brand)
-}).catch(error => console.error(error))
-
-OFFAPI.getLanguages().then(languages => {
-  console.info(languages)
-}).catch(error => console.error(error))
-
-OFFAPI.getLabels().then(labels => {
-  console.info(labels)
-}).catch(error => console.error(error))
-
-OFFAPI.getProductsByBarcodeBeginning('3596710').then(products => {
-  console.info(products)
-}).catch(error => console.error(error))
+  console.info(await worldOFF.getProduct(7622210288257))
+  console.info(await worldOFF.getBrand('monoprix'))
+  console.info(await worldOFF.getLanguages())
+  console.info(await worldOFF.getLabels())
+  console.info(await worldOFF.getProductsByBarcodeBeginning('3596710'))
+}
+try {
+  examples().then(() => {
+    console.info('Success')
+  })
+} catch (error) {
+  console.error(error)
+}
--- main.js
+++ main.js
@@ -1,58 +1,55 @@
 const request = require('request-promise')
 
+const defaultOptions = {
+  country: 'world'
+}
+
 class OFF {
-  constructor (options = {}) {
+  constructor (options = defaultOptions) {
     this.options = options
-    this.countryName = 'world'
-    this.URL = 'https://world.openfoodfacts.org'
+    this.URL = `https://${options.country}.openfoodfacts.org`
   }
-  country (countryName) {
-    this.countryName = countryName
-    this.URL = `https://${this.countryName}.openfoodfacts.org`
-    return this
+
+  setOption (option, value) {
+    return new OFF({
+      ...this.options,
+      [option]: value
+    })
+  }
+
+  country (country = defaultOptions.country) {
+    return this.setOption('country', country)
   }
 
   getBrands () {
-    const URL = this.URL
-    this.URL = 'https://world.openfoodfacts.org'
-    return request(`${URL}/brands.json`)
+    return request(`${this.URL}/brands.json`)
       .then(JSON.parse)
   }
 
   getProduct (barcode) {
-    const URL = this.URL
-    this.URL = 'https://world.openfoodfacts.org'
-    return request(`${URL}/api/v0/${barcode}`)
+    return request(`${this.URL}/api/v0/${barcode}`)
       .then(JSON.parse)
   }
 
   getBrand (brandName) {
-    const URL = this.URL
-    this.URL = 'https://world.openfoodfacts.org'
-    return request(`${URL}/brand/${brandName}.json`)
+    return request(`${this.URL}/brand/${brandName}.json`)
       .then(JSON.parse)
   }
 
   getLanguages () {
-    const URL = this.URL
-    this.URL = 'https://world.openfoodfacts.org'
-    return request(`${URL}/languages.json`)
+    return request(`${this.URL}/languages.json`)
       .then(JSON.parse)
   }
 
   getLabels () {
-    const URL = this.URL
-    this.URL = 'https://world.openfoodfacts.org'
-    return request(`${URL}/labels.json`)
+    return request(`${this.URL}/labels.json`)
       .then(JSON.parse)
   }
 
   getProductsByBarcodeBeginning (beginning) {
-    const URL = this.URL
-    this.URL = 'https://world.openfoodfacts.org'
     const fill = 'x'.repeat(13 - beginning.length)
     const barcode = beginning.concat(fill)
-    return request(`${URL}/code/${barcode}.json`)
+    return request(`${this.URL}/code/${barcode}.json`)
       .then(JSON.parse)
   }
 }

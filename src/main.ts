b0876c2c9d7ff56097b284b1c61dbb3e36537d65
ShaunakMishra25 <shaunakmishra374@gmail.com>
2025-06-25 21:38:32 +0530

    feat: add getProductImageFolder with test and const

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import createClient from "openapi-fetch";
-
 import {
   paths as pathsv2,
   components as componentsv2,
@@ -22,8 +21,8 @@ import {
   TaxoNode,
   Taxonomy,
 } from "./taxonomy/types";
-import { BackendType, BACKEND_DOMAINS, BACKEND_NAMES } from "./consts";
-const IMAGE_BASE_URL = "https://images.openfoodfacts.org/images/products/";
+import { BackendType, BACKEND_DOMAINS, BACKEND_NAMES, PRODUCT_IMAGE_BASE_URL as IMAGE_BASE_URL } from "./consts";
+
 export type ProductV2 = componentsv2["schemas"]["Product"];
 export type SearchResultV2 = externalv2["responses/search_for_products.yaml"];
 
@@ -127,33 +126,43 @@ export class OpenFoodFacts {
   getBrands(): Promise<Taxonomy<Brand>> {
     return this.getTaxo<Brand>("brands");
   }
+
   getLanguages(): Promise<Taxonomy<Language>> {
     return this.getTaxo<Language>("languages");
   }
+
   getLabels(): Promise<Taxonomy<Label>> {
     return this.getTaxo<Label>("labels");
   }
+
   getAdditives(): Promise<Taxonomy<Additive>> {
     return this.getTaxo<Additive>("additives");
   }
+
   getAllergens(): Promise<Taxonomy<Allergen>> {
     return this.getTaxo<Allergen>("allergens");
   }
+
   getCategories(): Promise<Taxonomy<Category>> {
     return this.getTaxo<Category>("categories");
   }
+
   getCountries(): Promise<Taxonomy<Country>> {
     return this.getTaxo<Country>("countries");
   }
+
   getIngredients(): Promise<Taxonomy<Ingredient>> {
     return this.getTaxo<Ingredient>("ingredients");
   }
+
   getPackagings(): Promise<Taxonomy<Ingredient>> {
     return this.getTaxo<Ingredient>("packaging");
   }
+
   getStates(): Promise<Taxonomy<State>> {
     return this.getTaxo<State>("states");
   }
+
   getStores(): Promise<Taxonomy<Store>> {
     return this.getTaxo<Store>("stores");
   }
@@ -204,14 +213,9 @@ export class OpenFoodFacts {
       },
     });
 
-    if (!res.data?.product) {
-      return null;
-    } else if (!res.data?.product?.images) {
-      return null;
-    }
+    if (!res.data?.product?.images) return null;
 
-    const imgObj = res.data?.product?.images;
-    return Object.keys(imgObj);
+    return Object.keys(res.data.product.images);
   }
 
   async search(
@@ -224,10 +228,15 @@ export class OpenFoodFacts {
 
     return res.data;
   }
+
+  /**
+   * Get base folder URL for a product's image
+   * @param productCode Barcode of the product
+   * @returns Folder path for the image files
+   */
   getProductImageFolder(productCode: string): string {
     const chunks = productCode.match(/.{1,3}/g);
     if (!chunks) return "";
-
     return `${IMAGE_BASE_URL}${chunks.join("/")}/`;
   }
 }

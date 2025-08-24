1cd9d710d4755c96f7f5f3494ba3cf3ced89578b
VaiTon <eyadlorenzo@gmail.com>
2025-08-24 19:13:50 +0200

    fix: add build step to Node.js workflow, fix tests setup
    Release-As: 2.0.0-alpha.15

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,15 @@
 import type { Config } from "jest";
 import { createDefaultPreset } from "ts-jest";
 
-const tsJestTransformCfg = createDefaultPreset({
-  tsconfig: "tsconfig.tests.json",
-}).transform;
-
 const config: Config = {
+  ...createDefaultPreset({
+    tsconfig: "tsconfig.tests.json",
+  }),
   preset: "ts-jest",
+  resolver: "ts-jest-resolver",
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
   verbose: true,
-  transform: {
-    ...tsJestTransformCfg,
-  },
 
   testMatch: ["**/tests/**/*.spec.ts"],
   coverageDirectory: "coverage",

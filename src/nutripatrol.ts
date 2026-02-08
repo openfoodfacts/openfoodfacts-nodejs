7c30ef1a67c9749e746a273748332202af1ceb1f
Ramneet Singh <ramneetsinghrtd@gmail.com>
2026-02-09 00:09:44 +0530

    exported component and path

--- src/nutripatrol.ts
+++ src/nutripatrol.ts
@@ -4,6 +4,8 @@ import type { components, paths } from "./schemas/nutripatrol.js";
 
 import { DEFAULT_NUTRIPATROL_API_URL, USER_AGENT } from "./consts.js";
 
+export type { components, paths } from "./schemas/nutripatrol.js";
+
 export class NutriPatrol {
   private readonly fetch: typeof global.fetch;
   private readonly baseUrl: string;

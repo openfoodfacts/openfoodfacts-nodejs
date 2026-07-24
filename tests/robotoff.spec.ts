7c92a7243fc820a3b083d9e728eb7cd7b543aa86
Rohan Singh <rohansingh.official@github.com>
2026-07-24 19:17:38 +0530

    style: fix Prettier formatting

--- tests/robotoff.spec.ts
+++ tests/robotoff.spec.ts
@@ -81,9 +81,9 @@ describe("Robotoff", () => {
     const res = await robotoff.loadLogo(testLogoId);
 
     expect(res).toEqual(mockData);
-     expect(fetchMock).toHaveBeenCalled();
-     const call = fetchMock.mock.calls[0];
-     const url = typeof call[0] === "string" ? call[0] : (call[0] as any).url;
-     expect(url).toContain(`/images/logos/${testLogoId}`);
-   });
+    expect(fetchMock).toHaveBeenCalled();
+    const call = fetchMock.mock.calls[0];
+    const url = typeof call[0] === "string" ? call[0] : (call[0] as any).url;
+    expect(url).toContain(`/images/logos/${testLogoId}`);
+  });
 });
